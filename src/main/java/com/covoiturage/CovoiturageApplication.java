240790e34886014bf3051180721b5a742b1a210b
bayendembaseye <bayendemba.seye@koonda.fr>
2019-09-01 11:50:54 +0000

    Add repository and entities

--- src/main/java/com/covoiturage/CovoiturageApplication.java
+++ src/main/java/com/covoiturage/CovoiturageApplication.java
@@ -2,12 +2,21 @@ package com.covoiturage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {CovoiturageApplication.class, Jsr310Converters.class})
 public class CovoiturageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CovoiturageApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(CovoiturageApplication.class, args);
-	}
-
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 }
